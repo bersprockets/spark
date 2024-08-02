28555556f15dbd9d8b2627ce38b74ba6da38c0c1
Bruce Robbins <bersprockets@gmail.com>
2024-08-02 14:21:29 -0700

    Update

--- sql/core/src/test/scala/org/apache/spark/sql/DatasetSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DatasetSuite.scala
@@ -2898,6 +2898,9 @@ class DatasetSuite extends QueryTest
       val factory = new MyEvalFactory
       assert(test.rdd.mapPartitionsWithEvaluator(factory).collect()(0) == "xfalse")
 
+      // test ZippedPartitionsWithEvaluatorRDD
+      assert(test.rdd.zipPartitionsWithEvaluator(test.rdd, factory).collect()(0) == "xfalse")
+
       // test collect transformation
       assert(test.rdd.collect {
         case x: String => x
